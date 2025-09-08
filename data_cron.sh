ec906d6b9f0d91857846039a951042a474f25908
maurya-anand <anandmaurya@hotmail.com>
2025-09-08 15:37:14 +0200

    added cron log file for monitoring

--- data_cron.sh
+++ data_cron.sh
@@ -6,9 +6,12 @@ FILE_LOOKUP=""
 DESTINATION=""
 SRC_DIR=""
 
+mkdir -p ${SRC_DIR}/db ${SRC_DIR}/logs
+CRON_LOG="${SRC_DIR}/logs/cron_status.log"
+
 find ${DATA_ROOT}/${RUN_FOLDER_PATTERN} -name ${FILE_LOOKUP} -type f | \
 while read file; do
     summary_dir=$(dirname "$file")
     run_dir=$(dirname $(dirname "$summary_dir"))
-    python3 ${SRC_DIR}/transfer.py --source "$run_dir" --target ${DESTINATION}
+    python3 ${SRC_DIR}/transfer.py --source "$run_dir" --target ${DESTINATION} >> ${CRON_LOG} 2>&1
 done
